dd29e31affa77a3074f15674c48de94742e6b3ca
dell <anjalisangekar1998@gmail.com>
2024-04-30 16:58:39 +0530

    Add project files.

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Left rotation");
+            var ar = new[] {1, 2, 3, 4, 5 };
+            Left(ar);
+            Console.WriteLine("Right rotation");
+            var arr = new[] { 1, 2, 3, 4, 5 };
+            Right(arr);
+        }
+
+        public static void Left(int[] a)
+        {
+            
+            int size = a.Length;
+            int temp;
+
+            for(int i = size-1; i > 0; i--)
+            {
+                temp = a[size - 1];
+                a[size - 1] = a[i - 1];
+                a[i - 1] = temp;
+            }
+
+            foreach(int num in a)
+            {
+                Console.Write(num + " ");
+               
+            }
+            Console.WriteLine();
+        }
+
+        
+        public static void Right(int[] a)
+        {
+            int size = a.Length;
+            int temp;
+
+            for(int i = 0; i < size-1; i++)
+            {
+                temp = a[0];
+                a[0] = a[i + 1];
+                a[i + 1] = temp; 
+            }
+
+            foreach(int num in a)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
